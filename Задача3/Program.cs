018c1e71c3969a8c56a6cbed8e9e77fc8b73695a
Alex <lrl.msk@yandex.ru>
2022-05-03 19:09:47 +0300

    add задача3

--- Задача3/Program.cs
+++ Задача3/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*  Написать программу- замена элемнтов массива: положительные элементы
+заменить на отрицательные и наоборот. */
+
+//функция которая заполняет массив рандом числами
+void Random(int[]mas)
+{
+    for (int i = 0; i < mas.Length; i++)
+    {
+        mas[i] = new Random().Next(-10, 10);
+    }
+}
+// функция замены 
+void Replace(int[]mas)
+{
+    for (int i = 0; i < mas.Length; i++)
+    {
+        mas[i] = -mas[i];
+    }
+}
+// Вывод массива на экран
+void Print(int[]mas)
+{
+    for (int i = 0; i < mas.Length; i++)
+    {
+        System.Console.Write(mas[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+int[] mas = new int [10];
+
+Random(mas);
+Print(mas);
+Replace(mas);
+Print(mas);
+
+
+
+
+
+
