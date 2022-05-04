73e4abc53931dc18af97b44ba60018381642f703
Alex <lrl.msk@yandex.ru>
2022-05-04 08:48:40 +0300

    add задача34 дз

---
+++ ДЗ/Задача34/Program.cs
@@ -0,0 +1,53 @@
+﻿/* Задача 34: Задайте массив заполненный случайными положительными 
+трёхзначными числами. Напишите программу, которая покажет количество 
+чётных чисел в массиве.
+[345, 897, 568, 234] -> 2  */
+
+Console.Clear();
+// заполняем массив рандом числами в диапазоне (100, 200);
+void Random(int[]massiv)
+{
+    for (int i = 0; i < massiv.Length; i++)
+    {
+        massiv[i] = new Random().Next(100, 200);
+    }
+}
+// вывод на экран
+void Print(int[]massiv)
+{
+    for (int i = 0; i < massiv.Length; i++)
+    {
+        System.Console.Write(massiv[i] +" ");
+    }
+    System.Console.WriteLine();
+}
+// проверка на четность
+int Count1(int[] mas)
+{
+    int result = 0;
+    for (int i = 0; i < mas.Length; i++)
+    {
+        if(mas[i] % 2 == 0) 
+          result += 1;
+    }
+    return result;
+}
+
+// проверка на нечетность
+int Count2(int[] mas)
+{
+    int result = 0;
+    for (int i = 0; i < mas.Length; i++)
+    {
+        if(mas[i] % 2 != 0) 
+          result += 1;
+    }
+    return result;
+}
+
+int[] massiv = new int [10];
+
+Random(massiv);
+Print(massiv);
+System.Console.WriteLine($"Количество четных чиел в массиве - {Count1(massiv)}");
+System.Console.WriteLine($"Количество нечетных чиел в массиве - {Count2(massiv)}");
--- Задача5/Program.cs
+++ Задача5/Program.cs
@@ -24,10 +24,10 @@ void Print(int[]mas)
 //
 void Play(int[]mas)
 {
-    int n1 = mas.Length; // можно укоротить mas.Length - заменить на n1
+    // int n1 = mas.Length; // можно укоротить mas.Length - заменить на n1
     
     int[]mas2 = new int[mas.Length/2 + mas.Length%2]; // создаем новый массив для перезаписи
-    int n2 = mas2.Length;  // можно укоротить mas.Length2 - заменить на n2
+    // int n2 = mas2.Length;  // можно укоротить mas.Length2 - заменить на n2
     for (int i = 0; i <= mas.Length / 2; i++) // дойдет до половины
     {
         mas2[i] = mas[i] * mas[mas.Length - 1 - i]; // перезаписываем в новый массив, также
