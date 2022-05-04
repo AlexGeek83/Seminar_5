fe69e5698b1ee45ceff5c2dd11df28d6e87e6e0f
Alex <lrl.msk@yandex.ru>
2022-05-04 20:32:43 +0300

    add задача36 дз

--- ДЗ/Задача36/Program.cs
+++ ДЗ/Задача36/Program.cs
@@ -4,5 +4,40 @@
 [-4, -6, 89, 6] -> 0 */
 
 
+Console.Clear();
+// заполняем массив рандом числами в диапазоне (-10, 30);
+void Random(int[]massiv)
+{
+    for (int i = 0; i < massiv.Length; i++)
+    {
+        massiv[i] = new Random().Next(-10, 30);
+    }
+}
+// вывод на экран
+void Print(int[]massiv)
+{
+    for (int i = 0; i < massiv.Length; i++)
+    {
+        System.Console.Write(massiv[i] +" ");
+    }
+    System.Console.WriteLine();
+}
+// проверка на нечетность
+int Sum(int[] massiv)
+{
+    int result = 0;
+    for (int i = 1; i < massiv.Length; i++)
+    {
+        if(i % 2 != 0) result += massiv[i];
+        
+    }
+    return result;
+}
+
+int[] massiv = new int [5];
+
+Random(massiv);
+Print(massiv);
+System.Console.WriteLine($"Сумма эл-тов в массиве на нечетных позициях = {Sum(massiv)}");
 
 
